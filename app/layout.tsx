8a0941d62e50ebd3c311ab194406773a35e39aa4
imranfeb <imranfeb@gmail.com>
2026-05-04 19:54:02 +0530

    fix: re-add GA4 gtag.js (GTM container only has deprecated UA, not GA4)

--- app/layout.tsx
+++ app/layout.tsx
@@ -86,6 +86,9 @@ export default function RootLayout({
   return (
     <html lang="en" className={inter.variable} suppressHydrationWarning>
       <head>
+        {/* Preconnect to critical origins */}
+        <link rel="preconnect" href="https://www.googletagmanager.com" />
+        <link rel="dns-prefetch" href="https://www.googletagmanager.com" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <meta name="theme-color" content="#2563eb" />
@@ -107,6 +110,16 @@ export default function RootLayout({
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-PZD76SQ');`}
         </Script>
         {/* End Google Tag Manager */}
+
+        {/* Google Analytics 4 - Direct (GTM container doesn't have GA4 configured yet) */}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}',{page_path:window.location.pathname});`}
+        </Script>
+        {/* End Google Analytics 4 */}
         <div className="min-h-screen flex flex-col">
           <Header />
           <main className="flex-1">{children}</main>
