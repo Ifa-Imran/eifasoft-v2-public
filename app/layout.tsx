5d3ade042fd597de89c67188b9cf8742e87b01e5
imranfeb <imranfeb@gmail.com>
2026-05-11 12:35:30 +0530

    refactor: remove direct GA4 gtag.js (now handled by GTM container)

--- app/layout.tsx
+++ app/layout.tsx
@@ -110,16 +110,6 @@ export default function RootLayout({
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-PZD76SQ');`}
         </Script>
         {/* End Google Tag Manager */}
-
-        {/* Google Analytics 4 - Direct (GTM container doesn't have GA4 configured yet) */}
-        <Script
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}',{page_path:window.location.pathname});`}
-        </Script>
-        {/* End Google Analytics 4 */}
         <div className="min-h-screen flex flex-col">
           <Header />
           <main className="flex-1">{children}</main>
